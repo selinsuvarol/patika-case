61517f0845976ec35de6a57fd4432133bfde7d2f
Selin Su Varol <selinsuvarol@gmail.com>
2022-09-25 21:12:55 +0300

    tailwind configuration

---
+++ final-case-selin-su-varol/public/components/Modal1.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Modal1 =
+    () => {
+        return (
+            <div className='bg-red-500'>
+                <h1>hhhdfhhhhh</h1>
+            </div>
+        )
+    }
+
+    export default Modal1;
